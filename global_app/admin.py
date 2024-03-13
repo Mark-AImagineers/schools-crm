10aff414683157a56a54810139f292fcc060afc7
Mark-AImagineers <markb@aimagineers.io>
2024-03-13 19:58:03 +1300

    .

--- global_app/admin.py
+++ global_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+@admin.register(Client)
+class TenantAdmin(admin.ModelAdmin):
+    list_display = ('tenant_name', 'tenant_updated_at')
+
+@admin.register(Domain)
+class DomainAdmin(admin.ModelAdmin):
+    list_display = ('domain_name', 'domain_updated_at')
