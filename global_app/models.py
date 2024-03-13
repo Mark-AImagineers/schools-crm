a428b09c05252cb222498f1b57163b79cc2fbd4e
Mark-AImagineers <markb@aimagineers.io>
2024-03-13 14:15:18 +1300

    .

--- global_app/models.py
+++ global_app/models.py
@@ -4,7 +4,7 @@ from django_tenants.models import TenantMixin, DomainMixin
 # Create your models here.
 
 class Client(TenantMixin):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255,default=False)
     created_on = models.DateTimeField(auto_now_add=True)
 
 class Domain(DomainMixin):
