6546dde0ccbded30bb28eb9b2ca88d4baedaa77e
Mark-AImagineers <markb@aimagineers.io>
2024-03-19 13:06:00 +1300

    .

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from dashboard.views import *
 
 urlpatterns = [
-    path('dash/', dashboard_view, name='dashboard'),
+    path('', dashboard_view, name='dashboard'),
 ]
